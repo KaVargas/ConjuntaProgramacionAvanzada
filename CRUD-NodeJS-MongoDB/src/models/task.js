048d800a62b7453981d761c2cfdb16e916ea5963
Kenedy Vargas <35229437+KaVargas@users.noreply.github.com>
2018-12-17 12:36:16 -0500

    Base of project

---
+++ CRUD-NodeJS-MongoDB/src/models/task.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const TaskSchema = new Schema({
+    title: String,
+    description: String,
+    status: {
+        type: Boolean,
+        default: false
+    }
+});
+
+const autorSchema = new Schema({
+    nombre: String
+});
+
+const bookSchema = new Schema({
+    title: String,
+    year: String,
+    autor: autorSchema
+});
+
+module.exports = mongoose.model('tasks',TaskSchema);
